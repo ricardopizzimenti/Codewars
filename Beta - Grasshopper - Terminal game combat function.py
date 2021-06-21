1ed41d9a8703d07e641ab8a584eaafaa25aa83db
ricardopizzimenti <85379723+ricardopizzimenti@users.noreply.github.com>
2021-06-21 16:44:51 -0300

    Beta - Grasshopper - Terminal game combat function

---
+++ Beta - Grasshopper - Terminal game combat function.py
@@ -0,0 +1,8 @@
+"""
+Create a combat function that takes the player's current health and the amount of damage received,
+and returns the player's new health.
+"""
+
+
+def combat(health, damage):
+    return health - damage
