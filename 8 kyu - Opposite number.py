e81fac96fe468c75b9edeed7cbfee6c650965a94
ricardopizzimenti <85379723+ricardopizzimenti@users.noreply.github.com>
2021-07-02 09:41:16 -0300

    8 kyu - Opposite number

---
+++ 8 kyu - Opposite number.py
@@ -0,0 +1,21 @@
+"""
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+"""
+
+
+# OPTION 1
+# def opposite(number):
+#    return number - number * 2
+
+# OPTION 2
+def opposite(number):
+    return -number
+
+
+print(opposite(1))
