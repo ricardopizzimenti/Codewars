518e988465221a47d93de18d98b4ed646db15d8b
Ricardo <ricardo.pizzimenti@hotmail.com>
2021-07-05 10:39:55 -0300

    8 kyu - Beginner - Lost Without a Map

---
+++ 8 kyu - Beginner - Lost Without a Map.py
@@ -0,0 +1,16 @@
+"""
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+"""
+
+
+def maps(a):
+    return [i * 2 for i in a]
+
+
+print(maps([1, 2, 3]))
