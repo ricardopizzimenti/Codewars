0913caa8acd61a5f8d7c3cf897a951bd1167a06d
Ricardo <ricardo.pizzimenti@hotmail.com>
2021-07-06 10:25:57 -0300

    8 kyu - Reversed Words

---
+++ 8 kyu - Reversed Words.py
@@ -0,0 +1,14 @@
+"""
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+"""
+
+
+def reverse_words(s):
+    return ' '.join(s.split()[::-1])
+
+
+print(reverse_words("hello world!"))
