d684670b1e95ddbf5c0dbd7782edbabad35f09bf
Ricardo <ricardo.pizzimenti@hotmail.com>
2021-07-10 09:17:43 -0300

    7 kyu - Sort arrays - 1

---
+++ 7 kyu - Sort arrays - 1.py
@@ -0,0 +1,11 @@
+"""
+Just a simple sorting usage.
+Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+"""
+
+
+def sortme(names):
+    return sorted(names)
+
+
+print(sortme(["one", "two", "three"]))
